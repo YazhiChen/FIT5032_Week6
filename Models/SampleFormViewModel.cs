0402dd2bcb3d0d153d114ebca7fa66a16dc86b35
Yazhi Chen <yche0338@student.monash.edu>
2020-09-08 22:18:53 +1000

    Add project files.

---
+++ Models/SampleFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_Week6.Models
+{
+    public class SampleFormViewModel
+    {
+       
+    }
+
+    public class FormOneViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.Display(Name = "First Name")]
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
